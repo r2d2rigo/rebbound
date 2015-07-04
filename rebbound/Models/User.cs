bd64f133b1c12ac140b2dbb172021d4ef474b90e
r2d2rigo <r2d2rigo@gmail.com>
2015-07-04 14:01:46 +0100

    Added basic version of User model.

---
+++ rebbound/Models/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Rebbound.Models
+{
+    [JsonObject()]
+    public class User
+    {
+        [JsonProperty("id")]
+        public int Id { get; private set; }
+
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+
+        [JsonProperty("username")]
+        public string Username { get; private set; }
+
+        // public string html_url { get; set; }
+
+        [JsonProperty("avatar_url")]
+        public string AvatarUrl { get; private set; }
+
+        [JsonProperty("bio")]
+        public string Bio { get; private set; }
+
+        [JsonProperty("location")]
+        public string Location { get; private set; }
+
+        //public Links links { get; set; }
+        //public int buckets_count { get; set; }
+        //public int comments_received_count { get; set; }
+        //public int followers_count { get; set; }
+        //public int followings_count { get; set; }
+        //public int likes_count { get; set; }
+        //public int likes_received_count { get; set; }
+        //public int projects_count { get; set; }
+        //public int rebounds_received_count { get; set; }
+        //public int shots_count { get; set; }
+        //public int teams_count { get; set; }
+        //public bool can_upload_shot { get; set; }
+        //public string type { get; set; }
+        //public bool pro { get; set; }
+        //public string buckets_url { get; set; }
+        //public string followers_url { get; set; }
+        //public string following_url { get; set; }
+        //public string likes_url { get; set; }
+        //public string projects_url { get; set; }
+        //public string shots_url { get; set; }
+        //public string teams_url { get; set; }
+        //public DateTime created_at { get; set; }
+        //public DateTime updated_at { get; set; }
+    }
+
+    //public class Links
+    //{
+    //    public string web { get; set; }
+    //    public string twitter { get; set; }
+    //}
+
+}
