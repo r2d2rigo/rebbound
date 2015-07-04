fff2e80470a6418c52c5fd581eed07c234526443
r2d2rigo <r2d2rigo@gmail.com>
2015-07-04 14:54:05 +0100

    Added BaseCredentials and BearerTokenCredentials for managing login credentials.

---
+++ rebbound/BearerTokenCredentials.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rebbound
+{
+    public class BearerTokenCredentials : BaseCredentials
+    {
+        private Dictionary<string, string> headers;
+
+        public string Token { get; private set; }
+
+        public BearerTokenCredentials(string token)
+        {
+            this.Token = token;
+        }
+
+        public override Dictionary<string, string> ToHttpHeaders()
+        {
+            if (this.headers == null)
+            {
+                this.headers = new Dictionary<string, string>();
+                this.headers.Add("Authorization", "Bearer " + this.Token);
+            }
+
+            return this.headers;
+        }
+    }
+}
